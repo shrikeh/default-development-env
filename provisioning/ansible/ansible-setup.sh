b377e8efec3ba055818df4cf83ab06b36647ad7a
Barney Hanlon <barney@shrikeh.net>
2017-09-07 20:17:00 +0100

    Move requirements file

--- provisioning/ansible/ansible-setup.sh
+++ provisioning/ansible/ansible-setup.sh
@@ -4,7 +4,7 @@ function setup_ansible_venv() {
   local virtualenv_dir='.venv'
   virtualenv "${virtualenv_dir}";
   source "${virtualenv_dir}/bin/activate";
-  pip install -q -r provisioning/requirements.txt;
+  pip install -q -r provisioning/ansible/requirements.txt;
 }
 
 setup_ansible_venv;
